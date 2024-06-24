43813a7dc1463084a615a680db727ea81f85e3b5
ishfaqmir <ishfaqmir@almuqeet.systems>
2024-06-24 10:44:21 +0530

    final push

--- app.js
+++ app.js
@@ -3,17 +3,19 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 const leadsRouter = require("./routes/leads");
 
 var app = express();
-app.set('trust proxy', true);
+app.set("trust proxy", true);
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -22,7 +24,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 const { PrismaClient } = require("@prisma/client");
 // const { Tokens } = require("../controllers/token-controller");
-
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
